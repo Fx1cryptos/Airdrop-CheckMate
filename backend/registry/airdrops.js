f48a3d07aecb7ce68ce5c064b64234514aba054c
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2026-01-08 03:21:24 +0100

    Create airdrops.js

---
+++ backend/registry/airdrops.js
@@ -0,0 +1,20 @@
+export const AIRDROPS = [
+  {
+    id: "base-activity",
+    name: "Base Ecosystem User",
+    chainId: 8453,
+    type: "token",
+    rules: {
+      minTx: 1
+    }
+  },
+  {
+    id: "kaito-yaps",
+    name: "Kaito Yaps (Speculative)",
+    chainId: 8453,
+    type: "points",
+    rules: {
+      social: "kaito"
+    }
+  }
+];
