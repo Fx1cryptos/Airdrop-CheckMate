7395615dc6efa1a954d941e70555e16e0ed52d13
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2026-01-08 02:39:37 +0100

    Create baseClient.js

---
+++ src/services/baseClient.js
@@ -0,0 +1,5 @@
+import { ethers } from "ethers";
+
+export const baseProvider = new ethers.JsonRpcProvider(
+  process.env.RPC_URL=https://base-mainnet.g.alchemy.com/v2/IIcMSrBxe9qC3niaD6AkfvICxIV6hO
+);
